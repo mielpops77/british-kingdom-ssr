ded51d309f133b8504b275c543ca2e93fa306625
mielpops77 <amielzeitoun@gmail.com>
2024-07-18 15:47:39 +0200

    test

--- src/app/services/cats/cats.service.ts
+++ src/app/services/cats/cats.service.ts
@@ -35,12 +35,14 @@ export class CatsService {
 
     // Avant de faire la requête HTTP, affichez le profilId et l'URL de la requête
     console.log('Profil ID:', profilId);
-    console.log('maaaaaaaaaaaaaaaj');
     const url = `${environment.urlCats}/${catId}`;
+    console.log('maaaaaaaaaaaaaaaj');
+    console.log('maaaaaaaaaaaaaaaj');
+
     console.log('URL de la requête:', url);
 
     // Effectuer la requête HTTP
-    return this.http.get<Cat>('https://british-kingdom-back.azurewebsites.net/api/cats/89', {
+    return this.http.get<Cat>(url, {
       params: { profilId: profilId.toString() }
     }).pipe(
       catchError(this.handleError)
